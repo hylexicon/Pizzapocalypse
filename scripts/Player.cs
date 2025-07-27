a05941e3dbc58b9b8a0324710537a2917a36cfe3
hylexicon <jimmyxie48@gmail.com>
2025-07-26 22:35:28 -0400

    feat: add player movement

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,10 +1,17 @@
 using Godot;
 using System;
 
-public partial class Player : CharacterBody2D
+public partial class Player : Character
 {
-    public override void _PhysicsProcess(double delta)
-    {
-        
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+
+		bool running = Input.IsActionPressed("run");
+		int speed = running? RunSpeed : WalkSpeed;
+		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+		Velocity = speed * inputDirection;
+
+		MoveAndSlide();
+	}
 }
