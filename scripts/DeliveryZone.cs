46d84815b6b8b96997583fec1ca6660ad0c67c9d
hylexicon <jimmyxie48@gmail.com>
2025-07-27 22:07:54 -0400

    feat: finished delivery zone

--- scripts/DeliveryZone.cs
+++ scripts/DeliveryZone.cs
@@ -3,4 +3,23 @@ using System;
 
 public partial class DeliveryZone : Area2D
 {
+	[Export]
+	private int payment = 4;
+
+	private GameManager gameManager;
+
+	public override void _Ready()
+	{
+		gameManager = GetNode<GameManager>("%GameManager");
+	}
+
+	public void OnBodyEntered(Node2D body)
+	{
+		if (body is Player player && player.HasPizza())
+		{
+			player.RemovePizza();
+			gameManager.AddMoney(payment);
+			QueueFree();
+		}
+	}
 }
--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class Player : Character
 {
@@ -8,10 +9,21 @@ public partial class Player : Character
 		base._PhysicsProcess(delta);
 
 		bool running = Input.IsActionPressed("run");
-		int speed = running? stats.RunSpeed : stats.WalkSpeed;
+		int speed = running ? stats.RunSpeed : stats.WalkSpeed;
 		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
 		Velocity = speed * inputDirection;
 
 		MoveAndSlide();
+	}
+
+	public bool HasPizza()
+	{
+		return HasItem("Pizza");
+	}
+
+	public void RemovePizza()
+	{
+		Item pizza = inventory.FirstOrDefault(item => item.Name == "Pizza");
+		RemoveItem(pizza);
 	}
 }
